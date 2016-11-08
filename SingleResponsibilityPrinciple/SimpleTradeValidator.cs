ded7e80080a843aa21a49e3e9c0d57a750f3031a
evangelista94 <echicheko@css.edu>
2016-11-08 16:37:05 -0600

    Request 403: Trade amounts must be between 1,000 and 100,000 units

--- SingleResponsibilityPrinciple/SimpleTradeValidator.cs
+++ SingleResponsibilityPrinciple/SimpleTradeValidator.cs
@@ -33,6 +33,13 @@ namespace SingleResponsibilityPrinciple
                 return false;
             }
 
+            if (tradeAmount<1000)
+            {
+                logger.LogWarning("Trade amount below 1000: '{0}'", tradeData[1]);
+                return false;
+            }
+
+
             decimal tradePrice;
             if (!decimal.TryParse(tradeData[2], out tradePrice))
             {
